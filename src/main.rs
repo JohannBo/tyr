738cdf65ccac11b853ff6bd938e0093a67a986f5
Johann Bornholdt <github@johann.bornholdt.online>
2018-04-26 21:56:24 +0200

    add basic main loop.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 extern crate tyr;
 
-fn main() {
+use std::io;
+use std::string::String;
 
-    if let Err(err) = tyr::write_csv() {
-        println!("Error: {}", err);
+fn main() {
+    println!("Running Tyr!");
+
+    loop {
+        println!("input command.");
+        println!("1: read");
+        println!("2; write");
+        println!("0: exit");
+
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command).expect("Failed to read line!");
+        let command = command.trim();
+        println!("command: {}", &command);
+
+        match command.as_ref() {
+            "0" => {
+                break;
+            }
+            "1" => {
+                println!("reading...")
+            }
+            "2" => {
+                println!("writing...")
+                if let Err(err) = tyr::write_csv() {
+                    println!("Error: {}", err);
+                }
+            }
+            _ => {
+                println!("invalid input.")
+            }
+        }
     }
 }
