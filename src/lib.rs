04231c42d43e92fda2a7ae0a0efaf71eaf4fb012
Johann Bornholdt <github@johann.bornholdt.online>
2018-04-27 12:56:46 +0200

    add datetime to record type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
+extern crate chrono;
 extern crate csv;
 #[macro_use]
 extern crate serde_derive;
 
+use chrono::prelude::*;
 use csv::Result;
 use std::fs::File;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Record {
     title: String,
-    start: String,
-    stop: Option<String>,
+    start: DateTime<Utc>,
+    stop: Option<DateTime<Utc>>,
 }
 
 pub fn read_csv() -> Result<()> {
-
     let file_buffer = File::open("/tmp/tyr_test.csv")?;
     let mut reader = csv::Reader::from_reader(file_buffer);
     for result in reader.deserialize() {
@@ -24,20 +25,24 @@ pub fn read_csv() -> Result<()> {
 }
 
 pub fn write_csv() -> Result<()> {
-
     let file_buffer = File::create("/tmp/tyr_test.csv")?;
     let mut wtr = csv::Writer::from_writer(file_buffer);
 
+    let start = Utc.ymd(2018, 4, 27).and_hms(10, 50, 0);
+    let stop = Utc::now();
+    let stop = stop.with_second(0).unwrap();
+    let stop = stop.with_nanosecond(0).unwrap();
+
     let record_1 = Record {
         title: "Ticket:1234,blah".to_string(),
-        start: "12:00".to_string(),
+        start: start,
         stop: None,
     };
 
     let record_2 = Record {
         title: "Ticket:1234".to_string(),
-        start: "12:00".to_string(),
-        stop: Some("18:00".to_string()),
+        start: start,
+        stop: Some(stop),
     };
 
     wtr.serialize(&record_1)?;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-extern crate chrono;
 extern crate tyr;
 
-use chrono::prelude::*;
 use std::io;
 use std::string::String;
 
@@ -28,14 +26,6 @@ fn main() {
                 if let Err(err) = tyr::write_csv() {
                     println!("Error: {}", err);
                 }
-            }
-            "3" => {
-                let now = Utc::now();
-                let now = now.with_second(0).unwrap();
-                let now = now.with_nanosecond(0).unwrap();
-                println!("now: {:?}", now);
-                let t = Utc.ymd(2018, 4, 27).and_hms(10, 50, 0);
-                println!("time: {:?}", t);
             }
             "h" => {
                 println!("commands:");
