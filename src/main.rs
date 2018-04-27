f155c7fa18bde5f2ba2a447d8944356e3108f43c
Johann Bornholdt <github@johann.bornholdt.online>
2018-04-27 11:12:04 +0200

    add chrono crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate chrono;
 extern crate tyr;
 
+use chrono::prelude::*;
 use std::io;
 use std::string::String;
 
@@ -17,12 +19,23 @@ fn main() {
             "0" => break,
             "1" => {
                 println!("reading...");
+                if let Err(err) = tyr::read_csv() {
+                    println!("Error: {}", err);
+                }
             }
             "2" => {
                 println!("writing...");
                 if let Err(err) = tyr::write_csv() {
                     println!("Error: {}", err);
                 }
+            }
+            "3" => {
+                let now = Utc::now();
+                let now = now.with_second(0).unwrap();
+                let now = now.with_nanosecond(0).unwrap();
+                println!("now: {:?}", now);
+                let t = Utc.ymd(2018, 4, 27).and_hms(10, 50, 0);
+                println!("time: {:?}", t);
             }
             "h" => {
                 println!("commands:");
